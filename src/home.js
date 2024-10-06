4002c022455e15432fbf987f3195085fcba82191
Raihan Sharif <raihansharif92@gmail.com>
2024-10-06 20:51:11 +0100

    Basic homepage made with Javascript
    The basic content of the home page generated with javascript

--- src/home.js
+++ src/home.js
@@ -1,5 +1,51 @@
+import addressImg from "./resources/odin.png";
+
 const content = document.getElementById("content");
 const title = document.createElement("h1");
 title.textContent = "Odin's mead hall!";
 
+const introContainer = document.createElement("div");
+const introPara = document.createElement("p");
+introPara.textContent = "Welcome, warriors and weary travelers, to Odin's hall! Feast,\
+    sing, and raise your horns to the Allfather. Let the warmth of the fire and the chee\
+    of the mead fill you, for tonight, you are among heroes and legends";
+
+introContainer.appendChild(introPara);
+
+const hoursContainer = document.createElement("div");
+const openingTimes = [
+    'Monday: 8am - 8pm',
+    'Tuesday: 8am - 8pm',
+    'Wednesday: 8am - 8pm',
+    'Thursday: 8am - 8pm',
+    'Friday: 8am - 8pm',
+    'Saturday: 8am - 8pm',
+    'Sunday: 8am - 8pm',
+]
+const hoursUl = document.createElement("ul");
+
+openingTimes.forEach(timeText => {
+    const li = document.createElement("li");
+    const textNode = document.createTextNode(timeText);
+    li.appendChild(textNode);
+    hoursUl.appendChild(li);
+});
+hoursContainer.appendChild(hoursUl);
+
+const addressContainer = document.createElement("div");
+const addressIcon = document.createElement("img");
+addressIcon.classList.add("icon");
+addressIcon.src = addressImg; //TODO: this ia placeholder image
+addressContainer.appendChild(addressIcon);
+
+
+const addressLine = document.createElement("p");
+addressLine.textContent = "123 Valhalla road, Asgard, Yggdrasil";
+addressContainer.appendChild(addressLine);
+// addressContainer.appendChild(document.createTextNode("123 Valhalla road, Asgard, Yggdrasil"));
+// addressContainer.textContent = "123 Valhalla road, Asgard, Yggdrasil";
+
 content.appendChild(title);
+content.appendChild(introContainer);
+content.appendChild(hoursContainer);
+content.appendChild(addressContainer);
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import "./styles.css";
 import {content} from "./home.js";
-import odinImg from "./resources/odin.png";
+// import odinImg from "./resources/odin.png";
 
-const image = document.createElement("img");
-image.src = odinImg;
+//const image = document.createElement("img");
+//image.src = odinImg;
 
-document.body.appendChild(image);
+// document.body.appendChild(image);
