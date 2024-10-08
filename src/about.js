8d967c47d1f5d08c6fe9517c86650cae39edb626
Raihan Sharif <raihansharif92@gmail.com>
2024-10-08 19:53:59 +0100

    About page done

--- src/about.js
+++ src/about.js
@@ -1,11 +1,27 @@
 const title = document.createElement("h1");
-title.textContent = "About us";
+title.textContent = "Meet the team";
 
+
+function teamMemberBuilder(name, description) {
+    const container = document.createElement("div");
+    const staffName = document.createElement("h2");
+    staffName.textContent = name;
+    const about = document.createElement('p');
+    about.textContent = description;
+    container.appendChild(staffName);
+    container.appendChild(about);
+    return container;
+}
+
+const moe = teamMemberBuilder("Moe Sizlack", "Moe is our handsome bartender, who is always pleased to see new faces.");
+const barney = teamMemberBuilder("Barney", "If you need advice on what to get from the bar, Barney is your man.");
+const ned = teamMemberBuilder("Ned", "This is the man to bring the fun. You'll never be bored when Ned is about.")
 
 const about = {
     title,
-    manager,
-    joe,
-    
+    moe,
+    barney,
+    ned,
+};
 
-}
+export { about };
--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import "./styles.css";
 import {home} from "./home.js";
 import {menu} from "./menu.js";
+import {about} from "./about.js";
 
 const content = document.getElementById("content");
 
 const homeButton = document.getElementById("home");
 const menuButton = document.getElementById("menu");
+const aboutButton = document.getElementById("about");
 
 homeButton.addEventListener("click", () => {
     content.innerHTML = "";
@@ -21,4 +23,13 @@ menuButton.addEventListener("click", () => {
     content.appendChild(menu.drinksContainer);
 });
 
+aboutButton.addEventListener("click", () => {
+    content.innerHTML = "";
+    content.appendChild(about.title);
+    content.appendChild(about.moe);
+    content.appendChild(about.barney);
+    content.appendChild(about.ned);
+});
+
 console.log(content);
+
